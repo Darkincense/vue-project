10cbb5e4fcecdd75f0d3cd14736d185d83162c50
Darkincense <1358338656@qq.com>
2018-02-23 13:34:25 +0800

    添加子组件

---
+++ src/components/view-chart/book-components/booktopComponent.vue
@@ -0,0 +1,12 @@
+<style lang="less" scoped>
+
+</style>
+<template>
+  <div class="app">
+      <h2>子页面</h2>
+  </div>
+</template>
+<script>
+    
+</script>
+
--- src/components/view-chart/book-form.vue
+++ src/components/view-chart/book-form.vue
@@ -2,15 +2,24 @@
 
 </style>
 <template>
-  <h2>这是一个全新的页面</h2>
+  <div>
+    <h2>这是一个全新的页面</h2>
+    <book-top></book-top>
+  </div>
 </template>
 <script>
+
+import bookTop from './book-components/booktopComponent';
+
 export default {
   data(){
     return {
       
     }
   },
+  components: {
+    bookTop
+  }
 }
 </script>
 
